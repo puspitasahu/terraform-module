12080ff015d9d820510bd0baa5095b9e889df6d8
puspitasahu <50769308+puspitasahu@users.noreply.github.com>
2024-12-16 09:51:59 +0000

    inital commit

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
 terraform{
     required_providers{
-        aws="hasicorp/aws"
+        aws ={
+            source="hasicorp/aws"
+            version ="~>5.0"
+        }        
+        
     }
+}
+
+provider "aws"{
+    region = var.aws_region
+}
+
+module "ec2_instance" {
+    source="./ec2"
+
 }
