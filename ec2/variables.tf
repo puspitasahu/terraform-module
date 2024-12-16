d35ff5e976cb457437d11dec3975267c9f76bb2c
puspitasahu <50769308+puspitasahu@users.noreply.github.com>
2024-12-16 10:15:53 +0000

    adding ec2 folder

---
+++ ec2/variables.tf
@@ -0,0 +1,9 @@
+varaible "instance_type" {
+    description = "EC2 instance type"
+    default = "t2.micro"
+}
+
+varaible "ami_id" {
+    description = "EC2 instance type"
+    default = "ami-0614680123427b75e"
+}
